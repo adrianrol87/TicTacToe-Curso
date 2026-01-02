32ded6a781a55491097a138ce0e852dc6e1e44ca
Adrian Rodriguez <adrianrol87@hotmail.com>
2026-01-01 18:00:06 -0600

    Created Xcode Project

---
+++ TicTacToe-TheOne-Curso/TicTacToe-TheOne-Curso/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe-TheOne-Curso
+//
+//  Created by Adrian Rodriguez Llorens on 01/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToe-TheOne-Curso/TicTacToe-TheOne-Curso/TicTacToe_TheOne_CursoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_TheOne_CursoApp.swift
+//  TicTacToe-TheOne-Curso
+//
+//  Created by Adrian Rodriguez Llorens on 01/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_TheOne_CursoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
